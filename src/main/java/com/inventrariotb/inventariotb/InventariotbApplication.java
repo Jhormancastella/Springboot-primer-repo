bff187142e33db22aa556c0423d1d4958497f083
jhormancastella <jesusjhorman123@gmail.com>
2025-04-28 16:12:45 -0500

    feat: :sparkles: primer commit de springboot

---
+++ src/main/java/com/inventrariotb/inventariotb/InventariotbApplication.java
@@ -0,0 +1,13 @@
+package com.inventrariotb.inventariotb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariotbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariotbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventrariotb/inventariotb/InventariotbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventrariotb.inventariotb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariotbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
